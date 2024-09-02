870a9d5126b031d43fd3bbe6299175a9a50c747b
Kushal <kushaldeveloper@gmail.com>
2024-09-02 13:05:54 -0400

    build application
    from the terminal

--- public/notes/download_video.ps1
+++ public/notes/download_video.ps1
@@ -3,15 +3,22 @@ param (
 )
 
 $tempDir = "C:\Temp\yt-dlp"
+date;
 New-Item -ItemType Directory -Path $tempDir -Force
 yt-dlp.exe --verbose --write-sub --sub-lang en --restrict-filenames --add-metadata -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4" --sponsorblock-mark all --sponsorblock-remove all --merge-output-format mp4 -o "$tempDir\%(id)s.%(ext)s" $url
+date;
 $videoFile = Get-ChildItem -Path $tempDir -Filter *.mp4 | Select-Object -First 1
+date;
 $subtitleFile = Get-ChildItem -Path $tempDir -Filter *.vtt | Select-Object -First 1
+date;
 if ($subtitleFile) {
     $subtitlePath = $subtitleFile.FullName
-    ffmpeg -i "$($videoFile.FullName)" -vf "subtitles='$subtitlePath':force_style='FontName=Arial,FontSize=24'" -c:v libx264 -c:a aac -strict experimental -movflags +faststart "$tempDir\$($videoFile.BaseName)_reencoded.mp4"
+    ffmpeg -loglevel debug -i "$($videoFile.FullName)" -vf "subtitles='$subtitlePath':force_style='FontName=Arial,FontSize=24'" -c:v libx264 -c:a aac -strict experimental -movflags +faststart "$tempDir\$($videoFile.BaseName)_reencoded.mp4"
 } else {
     ffmpeg -i "$($videoFile.FullName)" -c:v libx264 -c:a aac -strict experimental -movflags +faststart "$tempDir\$($videoFile.BaseName)_reencoded.mp4"
 }
+date;
 Move-Item -Path "$tempDir\$($videoFile.BaseName)_reencoded.mp4" -Destination "\\TP-Share\G\shared\jellyfin\shows\downloaded"
+date;
 Remove-Item -Path $tempDir -Recurse -Force
+date;
