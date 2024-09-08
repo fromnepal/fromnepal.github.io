606ab2f5b78f2511349f301b3bab4e59e4e32d6d
Kushal <kushaldeveloper@gmail.com>
2024-09-08 17:35:16 -0400

    build application
    from the terminal

--- docs/notes/2024/09/08/word.js
+++ docs/notes/2024/09/08/word.js
@@ -15,12 +15,16 @@ function findWordInGrid(grid, word) {
         return true;
     }
 
-    // Directions: right, down, down-right, down-left
+    // Directions: right, left, down, up, down-right, down-left, up-right, up-left
     const directions = [
         [0, 1],  // right
+        [0, -1], // left
         [1, 0],  // down
+        [-1, 0], // up
         [1, 1],  // down-right
-        [1, -1]  // down-left
+        [1, -1], // down-left
+        [-1, 1], // up-right
+        [-1, -1] // up-left
     ];
 
     for (let row = 0; row < rows; row++) {
