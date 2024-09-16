f9419ce910b3ccd79e4c340e32ef2285ed3d1418
Kushal <kushaldeveloper@gmail.com>
2024-09-15 20:39:31 -0400

    wip

--- public/notes/download_video.ps1
+++ public/notes/download_video.ps1
@@ -7,7 +7,7 @@ date;
 New-Item -ItemType Directory -Path $tempDir -Force
 dir $tempDir
 date;
-yt-dlp.exe --verbose --write-sub --sub-lang en --restrict-filenames --add-metadata -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4" --sponsorblock-mark all --sponsorblock-remove all --merge-output-format mp4 -o "$tempDir\%(id)s.%(ext)s" $url
+yt-dlp.exe --verbose --write-sub --sub-lang en --restrict-filenames --add-metadata -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4" --sponsorblock-mark all --sponsorblock-remove all --merge-output-format mp4 -o "$tempDir\%(title).50s.%(ext)s" $url
 date;
 $videoFile = Get-ChildItem -Path $tempDir -Filter *.mp4 | Select-Object -First 1
 date;
@@ -43,7 +43,7 @@ echo "Cleaned video path: $($videoFile.FullName)"
 
 if ($subtitleFile) {
     echo "Running ffmpeg with subtitles"
-    $ffmpegCommand = "ffmpeg -loglevel debug -i `"$($videoFile.FullName)`" -vf `subtitles='C\\:Temp\\yt-dlp\\$($subtitleFile.Name)':force_style='FontName=Arial,FontSize=24'` -c:v libx264 -c:a aac -strict experimental -movflags +faststart `"$tempDir\$($videoFile.BaseName)_reencoded.mp4`""
+    $ffmpegCommand = "ffmpeg -loglevel debug -i `"$($videoFile.FullName)`" -vf `subtitles='$($subtitlePath)':force_style='FontName=Arial,FontSize=24'` -c:v libx264 -c:a aac -strict experimental -movflags +faststart `"$tempDir\$($videoFile.BaseName)_reencoded.mp4`""
     echo $ffmpegCommand
     Invoke-Expression $ffmpegCommand
 } else {
