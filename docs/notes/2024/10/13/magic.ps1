c047700577bb0c56254001cfb72a9f7945ba33a9
Kushal <kushaldeveloper@gmail.com>
2024-10-16 19:19:17 -0400

    build application
    from the terminal

--- docs/notes/2024/10/13/magic.ps1
+++ docs/notes/2024/10/13/magic.ps1
@@ -2,7 +2,7 @@ Get-Date;
 # Define the directory containing HEIC files
 $sourceDir = "C:\Users\kushal\Pictures\heic"
 # Define the directory to save PNG files
-$destDir = "C:\Users\kushal\Pictures\print-2024-10-13"
+$destDir = "C:\Users\kushal\Pictures\2024-10-16"
 
 # Create the destination directory if it doesn't exist
 if (!(Test-Path -Path $destDir)) {
@@ -13,12 +13,13 @@ if (!(Test-Path -Path $destDir)) {
 $heicFiles = Get-ChildItem -Path $sourceDir -Filter *.heic
 
 foreach ($file in $heicFiles) {
+    Get-Date;
     # Define the output file path
     $outputFile = Join-Path -Path $destDir -ChildPath ($file.BaseName + ".png")
-    
+    Get-Date;
     # Convert HEIC to PNG using ImageMagick
     magick "$($file.FullName)" "$outputFile"
-    
+    Get-Date;
     # Copy metadata from HEIC to PNG
     magick mogrify -profile "$($file.FullName)" "$outputFile"
 }
