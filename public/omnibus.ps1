f0084c6e7f6905cc46fccb8e08c8fb7436f15c0d
Kushal <kushaldeveloper@gmail.com>
2024-10-31 15:17:53 -0400

    build application
    from the terminal

---
+++ public/omnibus.ps1
@@ -0,0 +1,166 @@
+$firefoxPath = "C:\Users\kushal\AppData\Local\Mozilla Firefox"
+$env:Path = "$firefoxPath;$env:Path"
+
+cd "C:\Users\kushal\src\angular\angulareighteen\"
+Get-Date
+Remove-Item -Recurse -Force .\node_modules
+Get-Date
+Remove-Item -Force .\yarn.lock
+Get-Date
+git fetch
+Get-Date
+git status
+Get-Date
+Get-Date
+git add .
+git commit --message "add all files" --message "update from terminal"
+Get-Date
+git pull --rebase origin master
+Get-Date
+git push origin master
+Get-Date
+yarn
+Get-Date
+yarn version --patch
+Get-Date
+yarn run ng update
+Get-Date
+yarn run ng build
+Get-Date
+Get-Date
+git fetch
+Get-Date
+git status
+Get-Date
+Get-Date
+git add .
+git commit --message "add all files" --message "update from terminal"
+Get-Date
+git pull --rebase origin master
+Get-Date
+git push origin master
+Get-Date
+yarn run ng test
+Get-Date
+yarn run ng update @angular/core @angular/cli @angular/material
+Get-Date
+git add .
+git commit --message "add all files" --message "update from terminal"
+Get-Date
+git pull --rebase origin master
+Get-Date
+git push origin master
+Get-Date
+echo y | npx update-browserslist-db@latest
+Get-Date
+git fetch
+Get-Date
+git status
+Get-Date
+Get-Date
+git add .
+git commit --message "add all files" --message "update from terminal"
+Get-Date
+git pull --rebase origin master
+Get-Date
+git push origin master
+Get-Date
+git remote show origin
+Get-Date
+
+cd "C:\Users\kushal\RustroverProjects\helloworld\"
+Get-Date
+cargo clean
+Get-Date
+cargo build
+Get-Date
+cargo test --profile test --no-fail-fast
+Get-Date
+git add .
+Get-Date
+git status
+Get-Date
+git commit --message "build application" --message "from the terminal" --verbose
+Get-Date
+git pull --rebase origin master --verbose
+Get-Date
+git push origin master --verbose
+Get-Date
+
+cd "C:\Users\kushal\src\mydotnet\"
+Get-Date
+dotnet clean
+Get-Date
+dotnet build
+Get-Date
+dotnet test
+Get-Date
+
+cd "C:\Users\kushal\src\mydotnet\tests\"
+Get-Date
+dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura
+reportgenerator -reports:coverage.cobertura.xml -targetdir:coverage-report
+Get-Date
+Move-Item -Path "C:\Users\kushal\src\mydotnet\tests\coverage-report\*" -Destination "C:\Users\kushal\src\mydotnet\docs" -Force
+Get-Date
+
+cd "C:\Users\kushal\src\mydotnet\"
+git add .
+Get-Date
+git status
+Get-Date
+git commit --message "build application" --message "from the terminal" --verbose
+Get-Date
+git pull --rebase origin master --verbose
+Get-Date
+git push origin master --verbose
+Get-Date
+
+cd "C:\Users\kushal\src\myhtml\matchthree\matchthree\"
+Get-Date
+yarn
+Get-Date
+yarn run build
+Get-Date
+git add .
+Get-Date
+git status
+Get-Date
+git commit --message "build application" --message "from the terminal" --verbose
+Get-Date
+git pull --rebase origin master
+Get-Date
+git push origin master
+Get-Date
+
+cd "C:\Users\kushal\src\mylogging\"
+Get-Date
+git add .
+Get-Date
+git commit --message "add all files" --message "from the terminal"
+Get-Date
+
+$imageUrl = "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/FD/GEOCOLOR/10848x10848.jpg"
+$outputPath = "C:\Users\kushal\src\myhtml\fromnepal\public\notes\earth.jpeg"
+Get-Date
+Invoke-WebRequest -Uri $imageUrl -OutFile $outputPath
+Get-Date
+Write-Output "Image downloaded successfully to $outputPath"
+Get-Date
+
+cd "C:\Users\kushal\src\myhtml\fromnepal"
+Get-Date
+yarn
+Get-Date
+yarn run build
+Get-Date
+git add .
+Get-Date
+git status
+Get-Date
+git commit --message "build application" --message "from the terminal" --verbose
+Get-Date
+git pull --rebase origin main
+Get-Date
+git push origin main
+Get-Date
