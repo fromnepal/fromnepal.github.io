0916e55a6adf0636effee5a3a9043c49ca196ff5
Kushal <kushaldeveloper@gmail.com>
2024-11-14 11:05:54 -0500

    build application
    from the terminal

--- public/omnibus.ps1
+++ public/omnibus.ps1
@@ -87,34 +87,7 @@ Get-Date
 git push origin master --verbose
 Get-Date
 
-cd "C:\Users\kushal\src\mydotnet\"
-Get-Date
-dotnet clean
-Get-Date
-dotnet build
-Get-Date
-dotnet test
-Get-Date
-
-cd "C:\Users\kushal\src\mydotnet\tests\"
-Get-Date
-dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura
-reportgenerator -reports:coverage.cobertura.xml -targetdir:coverage-report
-Get-Date
-Move-Item -Path "C:\Users\kushal\src\mydotnet\tests\coverage-report\*" -Destination "C:\Users\kushal\src\mydotnet\docs" -Force
-Get-Date
-
-cd "C:\Users\kushal\src\mydotnet\"
-git add .
-Get-Date
-git status
-Get-Date
-git commit --message "build application" --message "from the terminal" --verbose
-Get-Date
-git pull --rebase origin master --verbose
-Get-Date
-git push origin master --verbose
-Get-Date
+C:\Users\kushal\src\myhtml\fromnepal\public\mydotnet.ps1
 
 cd "C:\Users\kushal\src\myhtml\matchthree\matchthree\"
 Get-Date
